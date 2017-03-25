b59d4079357f7968b6401b5e063d0bafb6966648
Luke Brandl <brandl@umich.edu>
2017-03-25 18:28:48 -0400

    Fixes and tweaks

--- FitnessHelper.py
+++ FitnessHelper.py
@@ -10,7 +10,7 @@ import logging
 from random import randint
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
-from Classes.py import Exercise, ExerciseInstance, Routine
+from Classes import Exercise, ExerciseInstance, Routine
 
 app = Flask(__name__)
 
@@ -28,10 +28,13 @@ def launch():
     session.attributes['routine']=None
     session.attributes['routines'] = []
     session.attributes['uid'] = 1001
+    return question("Welcome to Fitness Helper")
 
+################### STATELESS INTENTS
 @ask.intent("CreateRoutineIntent", convert={'dayofweek':str,'X':int})
 def create_routine(dayofweek,X):
 
+    print ('creating routine')
     routine_name=dayofweek+' '+str(X)
     for routine in session.attributes['routines']:
         if routine.name==routine_name:
@@ -39,12 +42,23 @@ def create_routine(dayofweek,X):
             ##############################################
     new_routine = Routine(session.attributes['uid'],routine_name)
     session.attributes['uid']=session.attributes['uid']+1
-    session.attributes['routines'].append(new_routine)
-    
+    #session.attributes['routines'].append(new_routine)
     session.attributes['state']=MODIFYINGROUTINE
-    session.attributes['routine']=new_routine
-    return question('New routine created with name '+new_routine.name+' and id number '+new_routine.uid+\
-    '. Would you like to make any changes? Tell me when you are done modifying the routine')
+    #session.attributes['routine']=new_routine
+        
+    return question(render_template('TestIntent'))
+#    return question(render_template('CreateRoutineIntent',dayofweek=dayofweek,X=str(X)))
+    #'New routine created with name '+new_routine.name+' and id number '+str(new_routine.uid)+\
+#    '. Would you like to make any changes? Tell me when you are done modifying the routine')
+
+@ask.intent("ListRoutinesIntent")
+def list_routines():
+    
+    print('###################')
+    routines ='' 
+    for routine in session.attributes['routines']:
+        routines=routines+str(routine.name)+' \n'
+    return question(render_template('ListRoutinesIntent', routines=routines))
 
 if __name__ == '__main__':
 
