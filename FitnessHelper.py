907f551269f1ebf02b02b38f76a751d447046fef
Luke Brandl <brandl@umich.edu>
2017-03-26 01:08:16 -0400

    Still cant seem to get add instance to run?

--- FitnessHelper.py
+++ FitnessHelper.py
@@ -24,21 +24,34 @@ MODIFYINGROUTINE=1
 
 @ask.launch
 def launch():
-    session.attributes['state']=DEFAULTSTATE
+#    session.attributes['state']=DEFAULTSTATE
+    session.attributes['state']=MODIFYINGROUTINE
     session.attributes['routine']=None
     session.attributes['routines'] = []
     session.attributes['exercises']=[]
     session.attributes['uid'] = 1001
-    return question("Welcome to Fitness Helper")
+
+    #DEFAULTS
+    myexercise = Exercise(0,'Lunges','Quadriceps').__dict__ 
+    session.attributes['exercises'].append(myexercise)
+    myexercise = Exercise(1,'sit ups','Abdominals').__dict__ 
+    session.attributes['exercises'].append(myexercise)
+    myroutine = Routine(10,'Friday 7') .__dict__
+    session.attributes['routine']=myroutine
+#    myroutine['exercises'].append(ExerciseInstance('Lunges',4,10,30).__dict__)
+    myroutine['exercises'].append(ExerciseInstance('sit ups',3,50,30).__dict__)
+    session.attributes['routines'].append(myroutine)
+    
+    return question("Welcome to Fitness Assistant")
 
 ################### STATELESS INTENTS
 @ask.intent("CreateRoutineIntent", convert={'dayofweek':str,'X':int})
 def create_routine(dayofweek,X):
 
-    print ('creating routine')
+    print ('creating routine############################')
     routine_name=dayofweek+' '+str(X)
     for routine in session.attributes['routines']:
-        if routine.name==routine_name:
+        if routine['name']==routine_name:
             return question("Cannot create this routine, routine with same name already exists")
             ##############################################
     new_routine = Routine(session.attributes['uid'],routine_name)
@@ -76,7 +89,33 @@ def create_exercise(newexercise):
 
     return question(render_template('CreateExerciseIntent',exercise_name=exercise_name))
     
-
+@ask.intent("ListExercisesIntent")
+def list_exercises(): 
+    
+    #print('###################')
+    exercises ='' 
+    for exercise in session.attributes['exercises']:
+        exercises=exercises+str(exercise['name'])+' \n'
+    return question(render_template('ListExercisesIntent', exercises=exercises))  
+   
+   
+########## MODIFYING ROUTINE INTENTS
+@ask.intent("AddExerciseIntent",convert={'exercise':str,'setnum':int,'repnum':int})
+def add_exercise(newexercise,exercise,setnum,repnum):     
+    if(session.attributes['state']!=MODIFYINGROUTINE):
+        return question("You don't have an active routine to modify. Either chose a routine to modify or create a new routine")
+    exercise_name=exercise
+    myex=None
+    for ex in session.attributes['exercises']:
+        if ex['name']==exercise_name:
+            myex=ex
+    if myex==None:
+        return question("I couldn't find this exercise in your exercises. Have you created it yet?")
+    myinstance = ExerciseInstance(exercise_name,setnum,repnum,30)
+    session.attributes['routine']['exercises'].append(myinstance.__dict__)
+    return question(render_template('AddExerciseIntent', len(session.attributes['routine']['exercises'])))
+        
+            
 if __name__ == '__main__':
 
     app.run(debug=True)
